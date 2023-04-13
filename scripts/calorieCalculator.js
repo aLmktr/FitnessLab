86f9b79213d06bde78d6bf1f11349899c72d1955
aLmktr <almukhtaralhadhrami@gmail.com>
2023-04-13 13:56:10 +0400

    CalorieCalculator-v1 🧮

--- scripts/calorieCalculator.js
+++ scripts/calorieCalculator.js
@@ -1,3 +1,30 @@
 function calculate() {
-  var form = document.getElementById("claorieCalculator");
+  let form = document.forms["claorieCalculator"];
+  let weight = form.weight.value;
+  let height = form.height.value;
+  let age = form.Age.value;
+  let activity = form.activeity.value;
+  let gender = form.gender.value;
+  let result = document.getElementById("calorieResult");
+  result.innerHTML = "";
+
+  let calorieNeed = calorieEquation(weight, height, age, gender, activity);
+
+  result.innerHTML += `<p class="h3 text-dark">${calorieNeed} Calorie<p>`;
+  return false;
+}
+
+function calorieEquation(weight, height, age, gender, activity) {
+  // calculate the BMR
+  BMR = 10 * weight + 6.25 * height - 5 * age;
+  // change the parametres based on gender
+  if (gender == "male") {
+    BMR += 5;
+  } else if (gender == "female") {
+    BMR -= 161;
+  }
+
+  // calculate the final total daliy need with activity factor
+  TDEE = Math.round(BMR * activity);
+  return TDEE;
 }
