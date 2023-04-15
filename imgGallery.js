9cd5fcb0eef2d7bfa9b85c4fdbdc053b03a53cf2
aLmktr <almukhtaralhadhrami@gmail.com>
2023-04-16 00:10:58 +0400

    Dynamic photo gallery 📷⛓

--- imgGallery.js
+++ imgGallery.js
@@ -7,30 +7,50 @@ gallery.style.cssText = `display: flex; text-align: center; flex-wrap: wrap; jus
 // images sources
 
 let sources = {
-  img1: ["/src/noImg.webp", "This is the first image"],
-  img2: ["/src/noImg.webp", "This is the second image"],
-  img3: ["/src/noImg.webp", "This is the third image"],
+  img1: [
+    "/src/cardio.png",
+    "We offer customized cardio plans tailored to your fitness goals and level of fitness",
+  ],
+  img2: [
+    "/src/supplements.jpg",
+    "We offer expert advice on supplements for optimal health and wellness.",
+  ],
+  img3: [
+    "/src/calorie.png",
+    "We offer a calorie calculator service to help you track your daily intake",
+  ],
 };
 
-// display the images in the gallery
-function displayImages(sources) {
-  for (key in sources) {
-    // check if the property/key is defined in the object itself, not in parent
-    if (sources.hasOwnProperty(key)) {
-      let imgSrc = sources[key][0];
-      let description = sources[key][1];
-      let img = `<div class="gallery" style = "background-color: white; margin: 2em;">
+// display the image
+function displayImages(sources, key) {
+  let imgSrc = sources[key][0];
+  let description = sources[key][1];
+  let img = `<div class="gallery" style = "background-color: white; border-radius: 0.5em;">
             <img
              src="${[imgSrc]}"
              alt="no Imgae"
-             width="300"
-             height="250"
-                            />
-            <div class="description">${description}</div>
+             width = 100%
+             height = 100%;
+             style = "border-radius:0.5em 0.5em 0 0;"/>
+            <div style = "padding: 1em; fon">${description}</div>
             </div>`;
-      gallery.innerHTML += img;
-    }
-  }
+  gallery.innerHTML = img;
 }
 
-displayImages(sources);
+// change image each 5 seconds
+function changeImage() {
+  let i = 0;
+  let interval = 5000;
+  let keys = Object.keys(sources);
+
+  // display first image when the page loads
+  displayImages(sources, keys[i]);
+
+  let timer = setInterval(function () {
+    i = (i + 1) % keys.length;
+    let key = keys[i];
+    displayImages(sources, key);
+  }, interval);
+}
+
+changeImage();
