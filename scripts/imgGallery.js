073593d332cb21e3f1ef1b7a0eaea33031f1abf9
aLmktr <almukhtaralhadhrami@gmail.com>
2023-04-28 16:59:16 +0400

    animated image gallery with AngularJS 🌌

--- scripts/imgGallery.js
+++ scripts/imgGallery.js
@@ -1,56 +1,33 @@
-// select the gallery div
-let gallery = document.querySelector("#gallery");
+// define angular module/app
+let app = angular.module("galleryApp", ["ngAnimate"]);
 
-// set the style for the gallery
-gallery.style.cssText = `display: flex; text-align: center; flex-wrap: wrap; justify-content: center; align-items: center;`;
+// create angular controller and pass the $scope and $interval
+app.controller("GalleryCtrl", function ($scope, $interval) {
+  // create array of images
+  $scope.images = [
+    {
+      src: "src/cardio.jpg",
+      alt: "cardio",
+    },
+    {
+      src: "src/supplements.jpg",
+      alt: "supplements",
+    },
+    {
+      src: "src/calorie.jpg",
+      alt: "calorie",
+    },
+  ];
 
-// images sources
+  // keep track of current image
+  $scope.activeImage = 0;
 
-let sources = {
-  img1: [
-    "src/cardio.png",
-    "We offer customized cardio plans tailored to your fitness goals and level of fitness",
-  ],
-  img2: [
-    "src/supplements.jpg",
-    "We offer expert advice on supplements for optimal health and wellness.",
-  ],
-  img3: [
-    "src/calorie.png",
-    "We offer a calorie calculator service to help you track your daily intake",
-  ],
-};
+  // interate through images
+  $interval(function () {
+    $scope.activeImage = ($scope.activeImage + 1) % $scope.images.length;
+  }, 2500);
 
-// display the image
-function displayImages(sources, key) {
-  let imgSrc = sources[key][0];
-  let description = sources[key][1];
-  let img = `<div class="gallery" style = "background-color: white; border-radius: 0.5em;">
-            <img
-             src="${[imgSrc]}"
-             alt="no Imgae"
-             width = 100%
-             height = 100%;
-             style = "border-radius:0.5em 0.5em 0 0;"/>
-            <div style = "padding: 1em; fon">${description}</div>
-            </div>`;
-  gallery.innerHTML = img;
-}
-
-// change image each 5 seconds
-function changeImage() {
-  let i = 0;
-  let interval = 5000;
-  let keys = Object.keys(sources);
-
-  // display first image when the page loads
-  displayImages(sources, keys[i]);
-
-  let timer = setInterval(function () {
-    i = (i + 1) % keys.length;
-    let key = keys[i];
-    displayImages(sources, key);
-  }, interval);
-}
-
-changeImage();
+  $scope.isActive = function (image) {
+    return $scope.images.indexOf(image) === $scope.activeImage;
+  };
+});
