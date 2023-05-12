0cac35959f3485e70ab18fe30d3e7bc115fb9aa2
aLmktr <almukhtaralhadhrami@gmail.com>
2023-05-12 09:39:05 +0400

    php training table

--- training.php
+++ training.php
@@ -157,7 +157,66 @@
         <p class="display-6 mb-4">Strength 🏋️‍♂️</p>
         <!--Legs-->
         <div class="row">
-          <div class="col-md-6" id="legDayTable"></div>
+        <div class="col-md-6">
+              <?php 
+
+              // class to maintain information about one row 
+              class Exercise {
+                private $exercise;
+                private $setsReps;
+
+                // constructor
+                public function __construct($exercise, $setsReps){
+                  $this->exercise = $exercise;
+                  $this->setsReps = $setsReps;
+                }
+
+                // get sets and reps
+                public function getSetsReps(){
+                  return $this->setsReps;
+                }
+
+                // get exercise
+                public function getExercise(){
+                  return $this->exercise;
+                }
+              }
+
+              // array of objects to maintain info about exercises
+              $legDayExercies = [
+                new Exercise("Squats", "3 x 12"),
+                new Exercise("Deadlifts", "3 x 10"),
+                new Exercise("Lunges", "3 x max"),
+                new Exercise("Leg Press", "3 x 15"),
+                new Exercise("Calf Raises", "4 x 12"),
+                new Exercise("Leg Extension", "3 x 15, 12, 10"),
+                new Exercise("Glute-Ham Raises", "3 x 10")
+              ];
+
+              // display the exercises in a table
+              function displayExercises($data){
+                echo "<table class='table table-responsive table-striped table-borderless'>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th>Exercise</th>";
+                echo "<th>Sets & Reps</th>";
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                foreach($data as $exercise){
+                  echo "<tr>";
+                  echo "<td>" . $exercise->getExercise() . "</td>";
+                  echo "<td>" . $exercise->getSetsReps() . "</td>";
+                  echo "</tr>";
+                }
+                echo "</tbody>";
+                echo "</table>";
+              }
+
+              $display = displayExercises($legDayExercies);
+              ?>
+              
+            </div>
           <div class="col-md-6">
             <p class="h3">Day 1 / Legs</p>
             <p class="mt-4" style="font-family: inherit">
